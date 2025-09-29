bb698efe377402d244adaa09b21b765d7e116a56
Luigi9665 <ventriglia.luigi@hotmail.com>
2025-09-29 16:59:22 +0200

    add function onClick

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,21 +1,27 @@
 import ButtonComponent from "./Button";
 import ImageComponent from "./ImageComponent";
+import { useState } from "react";
 
 const GetStar = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toogleBox = () => {
+    setVisible(!visible);
+  };
+
   return (
     <div className="stellar">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+      <button className={visible ? "hide" : "show"} onClick={toogleBox}>
+        Start
+      </button>
 
-      <div className="container">
+      <div className={`container ${visible ? "show" : "hide"}`}>
         <ImageComponent
           src="https://images.unsplash.com/photo-1701378430426-5eebbf4593f9?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Astratto"
           width={100}
         />
-        <ButtonComponent textButton="Press me" />
+        <ButtonComponent className="noAnimation" textButton="Press me" />
         <ButtonComponent className="animated-button" textButton="Animated" />
       </div>
     </div>
